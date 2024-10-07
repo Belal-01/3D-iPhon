8a12993b14a7a7d490e33e8bd48fb3d9c1a1adcc
Belal-01 <belalkhobieh343@gmail.com>
2024-10-07 07:19:26 +0300

    fix th

--- src/components/ModleView.jsx
+++ src/components/ModleView.jsx
@@ -2,7 +2,7 @@ import { Html, OrbitControls, View } from '@react-three/drei'
 import React, { Suspense } from 'react'
 import { PerspectiveCamera } from '@react-three/drei'
 import Lights from './Lights'
-import IPhone from './iPhone'
+import IPhone from './IPhone'
 import * as THREE from 'three'
 import Loader from './Loader'
 
